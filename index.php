8cbf203ad0f01b16605ca2929385c8e293742fb4
everaldomatias <matiaseveraldo@gmail.com>
2019-11-03 15:34:37 -0300

    First sprint.

---
+++ index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE-edge">
+        
+        <!-- HTML Meta Tags -->
+        <title>Brasa Design || Seu site a poucos cliques!</title>
+        <meta name="description" content="Agência especializada em WordPress, criando sites com design exclusivo">
+        
+        <!-- Google / Search Engine Tags -->
+        <meta itemprop="name" content="Brasa Design">
+        <meta itemprop="description" content="Agência especializada em WordPress, criando sites com design exclusivo">
+        <meta itemprop="image" content="https://brasa.art.br/wp-content/uploads/sites/77/2017/11/logo-brasa-moderninho-v2.png">
+        
+        <!-- Facebook Meta Tags -->
+        <meta property="og:url" content="https://brasa.art.br/">
+        <meta property="og:type" content="website">
+        <meta property="og:title" content="Brasa Design">
+        <meta property="og:description" content="Agência especializada em WordPress, criando sites com design exclusivo">
+        <meta property="og:image" content="https://brasa.art.br/wp-content/uploads/sites/77/2017/11/logo-brasa-moderninho-v2.png">
+        
+        <!-- Twitter Meta Tags -->
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="Brasa Design">
+        <meta name="twitter:description" content="Agência especializada em WordPress, criando sites com design exclusivo">
+        <meta name="twitter:image" content="https://brasa.art.br/wp-content/uploads/sites/77/2017/11/logo-brasa-moderninho-v2.png">
+
+        <link rel="alternate" href="https://brasa.art.br/" hreflang="pt-br">
+        <link rel="shortcut icon" href="https://brasa.art.br/wp-content/uploads/2019/05/favicon-300x300.png" type="image/x-icon">
+        <link rel="icon" href="https://brasa.art.br/wp-content/uploads/2019/05/favicon-300x300.png" type="image/x-icon">
+
+        <style>
+        :root {
+            --brasa-green: #00566b;
+            --brasa-green-hover: #484848;
+        }
+        @font-face {
+            font-family: 'Ostrich Sans';
+            src: url('/assets/OstrichSans-Bold.ttf');
+        }
+        html {
+            margin: 0;
+            padding: 0;
+            scroll-behavior: smooth;
+        }
+        body {
+            background-color: #f0f0f0;
+        	color: var( --brasa-green );
+        	font-size: 16px;
+        	margin: 0;
+            padding-top: 140px;
+        }
+        .container {
+            margin: 0 auto;
+            max-width: 1170px;
+        }
+        nav.primary-menu {
+            background-color: #fff;
+            font-family: Ostrich Sans;
+            font-size: 1.6rem;
+            height: auto;
+            padding: 30px;
+            position: fixed;
+            top: 0;
+            width: 100%;
+        }
+        nav.primary-menu .logo {
+            float: left;
+            max-width: 80px;
+        }
+        nav.primary-menu ul {
+            float: left;
+            list-style: none;
+            margin-left: 50px;
+            padding-left: 0;
+        }
+        nav.primary-menu ul li {
+            display: inline-block;
+        }
+        nav.primary-menu ul li a {
+            color: var( --brasa-green );
+            padding: 15px 30px;
+            text-decoration: none;
+        }
+        nav.primary-menu ul li a:hover {
+            color: var( --brasa-green-hover );
+        }
+        #what-is .container div {
+            float: right;
+            width: 50%;
+        }
+        </style>
+
+    </head>
+
+    <body>
+        <header class="header-main">
+            <nav class="primary-menu">
+                <div class="container">
+                    <img src="https://brasa.art.br/wp-content/uploads/2019/05/favicon-300x300.png" alt="Logo da Brasa Design" class="logo">
+                    <ul>
+                        <li><a href="#">O que é?</a></li>
+                        <li><a href="#">Como funciona!</a></li>
+                        <li><a href="#">Quero um site!</a></li>
+                        <li><a href="#">Dúvidas</a></li>
+                    </ul>
+                </div><!-- /.container -->
+            </nav>
+        </header>
+
+        <section id="what-is">
+            <div class="container">
+                <div>
+                    <h2>O que é?</h2>
+                    <p>
+                    Sed sagittis sollicitudin vulputate. Vestibulum semper pretium pharetra. Nam turpis neque, sagittis at ipsum non, feugiat eleifend ipsum. Maecenas tristique eget magna eget sollicitudin. In rutrum blandit gravida. Sed quis tempus lacus, a ultrices est. Maecenas justo enim, mattis at varius condimentum.
+                    </p>
+                </div>
+            </div><!-- /.container -->
+        </section><!-- /.what-is -->
+         
+    </body>
+<html>
